32663b2b6bcacfe90625130bd2e6868a9f44fd58
nakaekana <dig.wax2@gmail.com>
2015-08-15 12:00:31 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
   end
+  
+  ## ここから追記
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+end
+
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+
+class MessagesController < ApplicationController
+  def index
+    # Message をすべて取得する
+    @messages = Message.all
+  end
 end
