e5f5282b0c463c524dc34324b2e2ed3b9d49b15c
nakaekana <dig.wax2@gmail.com>
2015-08-21 01:33:35 +0000

    add age

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ActiveRecord::Base
  validates :name , length: { maximum: 20 } , presence: true
  # 年齢は必須入力かつ0以上120以下
  validates_numericality_of :age , greater_than_or_equal_to: 0, less_than_or_equal_to: 120, presence: true
- # 内容は必須入力かつ2文字以上30文字以下
+ # 内容は必須入力かつ2文字以上30文字以内
  validates :body , length: {minimum: 2 , maximum: 30 }, presence: true
  
 end
